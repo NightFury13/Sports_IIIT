d7efa0242e599c70e0f774a0add6b88f1743b60a
NightFury13 <develop13mohit@gmail.com>
2016-09-08 15:30:05 +0530

    Release 1.0 : Website for sports'16 is up

--- sports16/js/main.js
+++ sports16/js/main.js
@@ -267,3 +267,43 @@ var wow = new WOW ({
 });
 wow.init();
 
+var button = $("a");
+button.data("text-original", button.text());
+button.on("click", function() {
+    var el = $(this);
+    if(el.text() == el.data("text-swap")) {
+	el.text(el.data("text-original"));
+    }
+    else {
+	el.data("text-original", el.text());
+	el.text(el.data("text-swap"));
+    }
+});
+
+/* Newly added */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
